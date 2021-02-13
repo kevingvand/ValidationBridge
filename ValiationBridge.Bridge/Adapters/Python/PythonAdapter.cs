32446df42f4e462cd286bdaddb624b10c88adde6
Kevin van Dommele <kevinvd@live.nl>
2021-02-13 14:50:07 +0100

    Added plan for LoadModule

--- ValiationBridge.Bridge/Adapters/Python/PythonAdapter.cs
+++ ValiationBridge.Bridge/Adapters/Python/PythonAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,37 @@ namespace ValiationBridge.Bridge.Adapters.Python
 
         public override List<string> LoadModule(string modulePath)
         {
+            var python = new PythonConsole("LOAD_MODULES");
 
+            if (!python.IsInstalled())
+            {
+                _logService.LogWarning($"Python is not installed or configured incorrectly, no Python modules can be loaded.");
+                return new List<string>();
+            }
+
+            FileAttributes modulePathAttributes = File.GetAttributes(modulePath);
+
+            if (modulePathAttributes.HasFlag(FileAttributes.Directory))
+                //TODO: Load Modules From Directory
+                //checkModuleScript = LoadModulesFromDirectory(modulePath);
+            else if (Path.GetExtension(modulePath) == ".py")
+                return new List<string>();
+            //TODO: load modules from file.
+            //checkModuleScript = LoadModuleFromFile(modulePath);
+            else
+                return new List<string>();
+
+            python.Start();
+
+
+
+            //TODO: craete python console for loading module
+
+            python.Stop();
 
             return null;
         }
+
+        public
     }
 }
